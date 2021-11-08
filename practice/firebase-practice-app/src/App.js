b88c18879cd5cbb5b6eee82d0654d06b30ae9258
secolema <seanclmn1@gmail.com>
2021-11-08 17:25:16 -0500

    changed firebase version

--- practice/firebase-practice-app/src/App.js
+++ practice/firebase-practice-app/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import React,{ useState, useEffect } from 'react';
-import { onSnapshot, collection} from 'firebase/firestore';
-import firebase from 'firebase/compat/app';
+import React, { useState,useEffect } from "react";
+import firebase from "./firebase"
+
 import './App.css';
 
-import db from './firebase'
 function App() {
-  const [users,setUsers]=useState([{}])
-  const [chats,setChats]=useState([{}])
+  const [users,setUsers]= useState([]);
+  const [loading,setLoading]=useState(false)
+
+  const ref = firebase.firestore().collection("users")
+
+
+  function getUsers(){
+    setLoading(true)
+    ref.onSnapshot((querySnapshot)=>{
+      const items = []
+      querySnapshot.forEach((doc)=>{
+        items.push(doc.data())
+      })
+      setUsers(items)
+      setLoading(false)
+    })
+  }
 
 
   useEffect(()=>{
-    onSnapshot(collection(db, "users"),(snapshot)=>{
-      console.log(snapshot.docs)
-    });
+    getUsers()
+    console.log(users)
+  }, []);
 
-  })
+  if (loading) {
+    return <p>loading! loading! leave me alone :)</p>
+  }
+
+
+
   return (
     <div className="App">
+      <p>hi</p>
 
+      { users.map((user)=><p key = {user.username} >{user.username}</p>)}
     </div>
   );
 }
--- practice/firebase-practice-app/src/firebase.js
+++ practice/firebase-practice-app/src/firebase.js
@@ -1,12 +1,6 @@
-// Import the functions you need from the SDKs you need
-import firebase from 'firebase/compat/app'
-import { getFirestore } from 'firebase/firestore';
-import 'firebase/storage'
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from 'firebase/app'
+import "firebase/firestore"
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCkwaZiSW8LxW-0kODvTxRSB3YzEL1wYwI",
   authDomain: "fir-practice-85948.firebaseapp.com",
@@ -16,7 +10,7 @@ const firebaseConfig = {
   appId: "1:77523700392:web:033684b4b7f0220d93509d"
 };
 
-// Initialize Firebase
-initializeApp(firebaseConfig);
 
-export default getFirestore();
+firebase.initializeApp(firebaseConfig)
+
+export default firebase
